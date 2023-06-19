0560a040a65673099feb25e7bb88df5b13e22144
owino <5364023-owino@users.noreply.replit.com>
2023-06-19 17:41:58 +0000

    Initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@3.0.2/dist/full.css" rel="stylesheet" type="text/css" />
+  </head>
+    <body data-theme="lemonade">
+        @include('partials.header')
+      
+
+        <div class="content">
+          @yield('content')
+            <!-- Your page-specific content here -->
+        </div>
+
+        @include('partials.footer')
+      <script src="https://cdn.tailwindcss.com"></script>
+
+    </body>
+</html>
